e40f6140cd8cfd281f550edeafe138d298d9b8c1
towczare <violeticus@gmail.com>
2022-07-17 09:49:43 +0200

    Init project

---
+++ src/main/java/pl/sda2/scrappingfun/scrapingfun/ScrapingFunApplication.java
@@ -0,0 +1,13 @@
+package pl.sda2.scrappingfun.scrapingfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapingFunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapingFunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sda2/scrappingfun/scrapingfun/ScrapingFunApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda2.scrappingfun.scrapingfun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapingFunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
